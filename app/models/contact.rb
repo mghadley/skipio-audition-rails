96ad701786373f720c602a12b942a2b3ac5f18c5
mghadley <matthew.g.hadley@gmail.com>
2017-10-22 14:12:36 -0600

    cleanup

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,11 +7,17 @@ class Contact
     loop.with_index do |_, i|
       page = i + 1
       url = BASE_URL + "&page=#{page}&per=100"
-      response = RestClient.get(BASE_URL).body
-      response_hash = JSON.parse(response).with_indifferent_access
-      total_pages ||= response_hash[:meta][:total_pages]
-      contacts += response_hash[:data]
-      break if page == total_pages
+      attempt_count = 0
+      begin
+        attempt_count += 1
+        response = RestClient.get(BASE_URL).body
+        response_hash = JSON.parse(response).with_indifferent_access
+        total_pages ||= response_hash[:meta][:total_pages]
+        contacts += response_hash[:data]
+        break if page == total_pages
+      rescue
+        attempt_count < 5 ? retry : (return [])
+      end
     end
     return contacts
   end
